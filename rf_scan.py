fab8fa0e1d46a943aa3bb1bfe797886aa40e593f
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-26 08:57:06 +0900

    Update rf_scan.py

--- rf_scan.py
+++ rf_scan.py
@@ -1,12 +1,17 @@
-import RPi.GPIO as GPIO
+import lgpio
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN)
+LED_PIN = 27
+
+# GPIO 칩 핸들 생성 및 열기
+h = lgpio.gpiochip_open(0)
+
+# GPIO 핀을 입력으로 설정
+lgpio.gpio_claim_input(h, LED_PIN)
 
 def read_signal():
     # 신호를 읽고 이를 문자열로 반환
-    return str(GPIO.input(27))
+    return str(lgpio.gpio_read(h, LED_PIN))
 
 def detect_repeated_signals(required_repeats=3):
     last_signal = None
@@ -27,4 +32,5 @@ def detect_repeated_signals(required_repeats=3):
 try:
     detect_repeated_signals(required_repeats=5)  # 여기서 반복 횟수를 설정
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    # Ctrl+C를 누르면 실행 종료 및 GPIO 칩 핸들 닫기
+    lgpio.gpiochip_close(h)

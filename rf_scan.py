84e26847b7c1d2d809d7bde3d6a3f7bab6e71981
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-26 09:24:03 +0900

    Update rf_scan.py

--- rf_scan.py
+++ rf_scan.py
@@ -9,16 +9,13 @@ h = lgpio.gpiochip_open(0)
 # GPIO 핀을 입력으로 설정
 lgpio.gpio_claim_input(h, LED_PIN)
 
-def read_signal():
-    # 신호를 읽고 이를 문자열로 반환
-    return str(lgpio.gpio_read(h, LED_PIN))
-
 try:
-    # 초기 상태와 변화 감지
-    initial_state = read_signal()
+    initial_state = lgpio.gpio_read(h, LED_PIN)
     print("Initial pin state:", initial_state)
+    
+    # 상태 변화 감지를 위한 루프
     while True:
-        current_state = read_signal()
+        current_state = lgpio.gpio_read(h, LED_PIN)
         if current_state != initial_state:
             print(f"State changed to {current_state}")
             break

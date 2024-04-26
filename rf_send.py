7aa8976197de252a211bffeabb2caae1f4b09599
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-26 09:06:49 +0900

    Update rf_send.py

--- rf_send.py
+++ rf_send.py
@@ -1,12 +1,18 @@
-import RPi.GPIO as GPIO
+import lgpio
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+LED_PIN = 17
+
+# GPIO 칩 핸들 생성 및 열기
+h = lgpio.gpiochip_open(0)
+
+# GPIO 핀을 출력으로 설정
+lgpio.gpio_claim_output(h, LED_PIN)
 
 def send_signal(signal):
     for bit in signal:
-        GPIO.output(17, int(bit))
+        # 신호 전송 (0 또는 1)
+        lgpio.gpio_write(h, LED_PIN, int(bit))
         time.sleep(0.1)  # 간단한 딜레이로 비트를 전송합니다.
 
 button_signals = {
@@ -24,4 +30,5 @@ try:
         else:
             print("Invalid button")
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    # Ctrl+C를 누르면 실행 종료 및 GPIO 칩 핸들 닫기
+    lgpio.gpiochip_close(h)
